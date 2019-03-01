050061254d9dbd5039468e14c1353ba19f3ac627
U160709027 <oguzhansahin3@posta.mu.edu.tr>
2019-03-01 19:12:02 +0300

    lab1

---
+++ lab1/lab0.py
@@ -0,0 +1,15 @@
+import os
+import os.path,time
+
+os.chdir("/home")
+os.mkdir("os_lab_0")
+os.chdir("/home/os_lab_0")
+open("zuerst.txt","w")
+open("zweite.txt","w")
+open("dritte.py","w")
+print("Last modified of zuerst.txt : %s " % time.ctime(os.path.getmtime("zuerst.txt")))
+print("Last modified of zweite.txt : %s " % time.ctime(os.path.getmtime("zweite.txt")))
+print("Last modified of dritte.py : %s " % time.ctime(os.path.getmtime("dritte.py")))
+for file in os.listdir("/home/os_lab_0"):
+	if file.endswith(".txt"):
+		print(os.path.join("/home",file))
